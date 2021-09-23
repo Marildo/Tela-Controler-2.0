4009134efd892ceaf3ce82aa0c202a78cc8d4e74
Marildo Cesar <marildocesar@hotmail.com>
2021-09-23 06:50:52 -0300

    Feature(Retaguarda): Suavizando transicao de menu

--- frontend/tela-retaguarda/src/app/template/menu/menu.component.ts
+++ frontend/tela-retaguarda/src/app/template/menu/menu.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core'
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit, SimpleChange } from '@angular/core'
 
 @Component({
   selector: 'ut-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger('toggle',[
+      state('small', style({
+          width: 60
+      })),
+      state('large', style({
+        width: 200
+    })),
+    transition('large <=> small', animate('.6s ease-in-out'))
+    ])
+  ]
 })
 export class MenuComponent implements OnInit {
 
-  constructor() {
 
+  @Input() public expandMenu: Boolean;
+  public toggleMenu: string = 'large'
+
+  constructor() {
+    this.expandMenu = true
   }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: { [property: string]: SimpleChange }){
+    let changeExpandMenu: SimpleChange = changes['expandMenu']; 
+    this.toggleMenu = changeExpandMenu.currentValue ? 'large' : 'small'   
+ }
+
 }
+
+
+// TODO - Alterar comunicacao entre componentes para service
